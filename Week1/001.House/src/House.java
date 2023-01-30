a1600a04ccca83a48f09eb2e234bb2e391f3706f
Luc van der Sar <Lucvds2001@Gmail.com>
2023-01-30 15:01:37 +0100

    upload week 1 opdracht 1

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -5,6 +5,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
+import javax.sound.sampled.Line;
+import java.awt.geom.Line2D;
+
 public class House extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -17,6 +20,32 @@ public class House extends Application {
 
 
     public void draw(FXGraphics2D graphics) {
+        //floor
+        graphics.draw(new Line2D.Double(200, 500, 500, 500));
+        //right wall
+        graphics.draw(new Line2D.Double(500, 300, 500, 500));
+        //left wall
+        graphics.draw(new Line2D.Double(200, 300, 200, 500));
+        //left roof
+        graphics.draw(new Line2D.Double(200, 300, 350, 100));
+        //right roof
+        graphics.draw(new Line2D.Double(500, 300, 350, 100));
+
+        //inner parts of house
+        //left side door
+        graphics.draw(new Line2D.Double(240, 500, 240, 380));
+        //right side door
+        graphics.draw(new Line2D.Double(300, 500, 300, 380));
+        //top side door
+        graphics.draw(new Line2D.Double(240, 380, 300, 380));
+        // left side window
+        graphics.draw(new Line2D.Double(325, 450, 325, 350));
+        // right side window
+        graphics.draw(new Line2D.Double(475, 450, 475, 350));
+        //bottom side window
+        graphics.draw(new Line2D.Double(325, 450, 475, 450 ));
+        //top side window
+        graphics.draw(new Line2D.Double(325, 350, 475, 350));
     }
 
 
