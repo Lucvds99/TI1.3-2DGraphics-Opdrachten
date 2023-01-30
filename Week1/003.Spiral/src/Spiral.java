5c9c252f4979509fda29b0806af7f068d9901cf1
Luc van der Sar <Lucvds2001@Gmail.com>
2023-01-30 16:30:59 +0100

    Week1 assingment 1.3

--- Week1/003.Spiral/src/Spiral.java
+++ Week1/003.Spiral/src/Spiral.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Spiral extends Application {
+    Canvas canvas = new Canvas();
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Spiral");
@@ -20,6 +21,29 @@ public class Spiral extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(canvas.getWidth()/2, canvas.getHeight()/2);
+        graphics.scale( 1, -1);
+
+        graphics.setColor(Color.red);
+        graphics.drawLine(0,0,1000,0);
+        graphics.setColor(Color.green);
+        graphics.drawLine(0,0,0,1000);
+        graphics.setColor(Color.black);
+
+        float scale = 50;
+        float n = 0.1f;
+        float LastX = 0;
+        float LastY = 0;
+
+        for (float i = 0; i < 100; i+=0.1f) {
+            float x = (float)(n * i * Math.cos(i));
+            float y = (float)(n * i * Math.sin(i));
+
+            graphics.draw(new Line2D.Double(x * scale, y * scale, LastX * scale, LastY * scale));
+            LastX = x;
+            LastY = y;
+        }
+
     }
     
     
