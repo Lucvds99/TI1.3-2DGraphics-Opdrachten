5b3ca67e9665c0cd055f38ec6209e512b29a1847
= <63397031+Lucvds99@users.noreply.github.com>
2023-01-30 23:03:38 +0100

    Week1 assignmetn 1.4 Rainbow

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -9,17 +9,41 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Rainbow extends Application {
+    Canvas canvas = new Canvas();
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Rainbow");
         primaryStage.show();
     }
-    
-    
+
+
+
     public void draw(FXGraphics2D graphics) {
+
+        //sets the origin to middle
+        graphics.translate(1920/2,1000);
+        graphics.scale(1,-1);
+
+        // the amount of steps its going to take with ur for loop
+        float resolution = (float)0.0001;
+        //  with radius in and out you set the with of the arch
+        float radiusBinnen = (float) 600;
+        float radiusBuiten = (float) 700;
+
+        //the increment is the amount of color spread over the sphere
+        float increment = ((float)500.0/((float)Math.PI)*(float)(Math.PI));
+        for(float i = 0; i < Math.PI; i+= resolution) {
+            graphics.setColor(Color.getHSBColor(i*increment/500f, 1, 1));
+            float x1 = radiusBinnen * (float)Math.cos(i);
+            float y1 = radiusBinnen * (float)Math.sin(i);
+            float x2 = radiusBuiten * (float)Math.cos(i);
+            float y2 = radiusBuiten * (float)Math.sin(i);
+            graphics.draw(new Line2D.Double(x1,y1,x2,y2));
+        }
+
 
     }
     
