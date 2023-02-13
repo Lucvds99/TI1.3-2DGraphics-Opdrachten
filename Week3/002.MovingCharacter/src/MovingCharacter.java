a9c81ac7a65299f6d4d9141ce5d91a009a60fa3b
Luc van der Sar <Lucvds2001@Gmail.com>
2023-02-13 16:05:34 +0100

    Week3 Assignment MovingCharacter

--- Week3/002.MovingCharacter/src/MovingCharacter.java
+++ Week3/002.MovingCharacter/src/MovingCharacter.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -10,6 +11,7 @@ import static javafx.application.Application.launch;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -20,21 +22,41 @@ import org.jfree.fx.ResizableCanvas;
 
 public class MovingCharacter extends Application {
     private ResizableCanvas canvas;
+    private BufferedImage image;
+    private int spriteIndex = 42;
+    private boolean isJumping = false;
+
+    private int startSpriteIndex = 42;
+    private int endSpriteIndex = 50;
+    private int xPos = 0;
+    private int yPos = 0;
+    private double deltaTimer = 0;
+
+    private AffineTransform character;
+    private boolean backwards = false;
+
+    private BufferedImage[] tiles;
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
+        try {
+            image = ImageIO.read(getClass().getResource("/images/sprite.png"));
+            tiles = new BufferedImage[65];
+            for (int i = 0; i < 65; i++)
+                tiles[i] = image.getSubimage(64 * (i % 8), 64 * (i / 8), 64, 64);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         BorderPane mainPane = new BorderPane();
-        canvas = new ResizableCanvas(g -> draw(g), mainPane);
+        canvas = new ResizableCanvas(this::draw, mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
         new AnimationTimer() {
             long last = -1;
 
             @Override
-            public void handle(long now)
-            {
+            public void handle(long now) {
                 if (last == -1)
                     last = now;
                 update((now - last) / 1000000000.0);
@@ -50,20 +72,68 @@ public class MovingCharacter extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
+        graphics.setBackground(Color.darkGray);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        character = new AffineTransform();
+        character.translate(xPos, yPos);
+
+        canvas.setOnMousePressed(e -> mousePressed(e));
+
+        if (backwards)
+            character.scale(-1, 1);
+
+        graphics.drawImage(tiles[spriteIndex], character, null);
     }
 
 
-    public void update(double deltaTime)
-    {
+    public void update(double deltaTime) {
+
+            deltaTimer += deltaTime;
+            if (spriteIndex == endSpriteIndex)
+                spriteIndex = startSpriteIndex;
+
+
+            if (xPos > canvas.getWidth() - tiles[spriteIndex].getWidth()) {
+                backwards = true;
+            }
+            if (xPos < tiles[spriteIndex].getWidth()) {
+                backwards = false;
+            }
+
+            if (!backwards)
+                xPos += 4;
+            else
+                xPos -= 4;
+
+
+            if (deltaTimer > 0.06) {
+                spriteIndex++;
+                deltaTimer = 0;
+            }
+        if (isJumping){
+            if (spriteIndex == 25){
+                isJumping = false;
+                spriteIndex = 42;
+
+
+            }
+            if (!backwards)
+                xPos -= 4;
+            else
+                xPos += 4;
+        }
     }
 
-    public static void main(String[] args)
+    private void mousePressed(MouseEvent e)
     {
+        isJumping = true;
+        spriteIndex = 17;
+    }
+
+    public static void main(String[] args) {
         launch(MovingCharacter.class);
     }
 
